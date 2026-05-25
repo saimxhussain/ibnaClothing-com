de2c5357a2e3d8107f6869adfc063756e7a942e7
Saim Hussain <saimxhussain@gmail.com>
2026-05-26 02:45:44 +0500

    Delete src directory

--- src/components/CartToast.js
+++
@@ -1,7 +0,0 @@
-'use client';
-import { useCart } from '@/context/CartContext';
-
-export default function CartToast() {
-  const { toast } = useCart();
-  return <div className={`cart-toast ${toast ? 'show' : ''}`}>{toast && `✓  ${toast}`}</div>;
-}
